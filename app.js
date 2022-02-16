f1ba084f42bf9dca5107adf7e3b341b2c1d78f4a
Bornak <bornakpaul@gmail.com>
2022-02-16 09:39:11 +0530

    Previous practice code shifted to new file

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-//* GLOBALS
-
-//* __dirname   - path to currrent directory
-//* __filename  - file name
-//* require     - function to use modules (CommonJS)
-//* module      - info about current module (file)
-//* process     - info about env where the program is being executed
-
-console.log(__dirname);
-
-setInterval(()=>{
-    console.log('Hello World')
-}, 1000)
+//* Modules
